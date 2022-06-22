4e8459cba9395b9efc8089afde1b80b92e58da82
ic005k <zh99889@126.com>
2022-06-22 10:07:31 +0800

    Add files via upload

--- dlgtodo.cpp
+++ dlgtodo.cpp
@@ -5,7 +5,7 @@
 #include "ui_dlgtodo.h"
 #include "ui_mainwindow.h"
 
-QString highLblStyle = "background-color: rgb(255, 239, 219);color:black";
+QString highLblStyle = "background-color: rgb(65, 65, 245);color:white";
 int highCount;
 QString orgLblStyle;
 QListWidget *mylist, *listRecycle;
