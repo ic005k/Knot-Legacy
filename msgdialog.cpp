708ea20519f2392d69a7c2e5a0cd261b5d613ff5
ic005k <zh99889@126.com>
2022-08-15 10:18:34 +0800

    Add files via upload

--- msgdialog.cpp
+++ msgdialog.cpp
@@ -4,6 +4,7 @@
 #include "ui_msgdialog.h"
 
 extern MainWindow* mw_one;
+QString hsStyle;
 
 msgDialog::msgDialog(QWidget* parent) : QDialog(parent), ui(new Ui::msgDialog) {
   ui->setupUi(this);
@@ -11,29 +12,28 @@ msgDialog::msgDialog(QWidget* parent) : QDialog(parent), ui(new Ui::msgDialog) {
   this->layout()->setMargin(0);
   this->installEventFilter(this);
   initDlg();
+  ui->frameDate->hide();
+  ui->frameTime->hide();
+  // gl = new QGridLayout(this);
+  // gl->setMargin(10);
+  // gl->setSpacing(10);
+  // ui->frameSel->setLayout(gl);
 
-  // this->show();
+  this->show();
 }
 
 msgDialog::~msgDialog() { delete ui; }
 
 void msgDialog::initDlg() {
-  ui->lblText->setWordWrap(true);
-  ui->lblText->adjustSize();
+  ui->hsHour->setStyleSheet(hsStyle);
+  ui->hsMinute->setStyleSheet(hsStyle);
+  ui->hsYear->setStyleSheet(hsStyle);
+  ui->hsMonth->setStyleSheet(hsStyle);
+  ui->hsDay->setStyleSheet(hsStyle);
 
-  this->setFixedHeight(300);
-  this->setFixedWidth(mw_one->width());
-  this->setGeometry(mw_one->geometry().x(),
-                    (mw_one->height() - this->height()) / 2, this->width(),
-                    this->height());
+  this->setGeometry(mw_one->geometry().x(), mw_one->geometry().y(),
+                    mw_one->width(), mw_one->height());
   this->setModal(true);
-  ui->lblTime->setText(QDateTime::currentDateTime().toString());
-
-  QFont font(mw_one->font());
-  int size = font.pointSize();
-  font.setPointSize(size * 1.1);
-  font.setBold(true);
-  ui->lblText->setFont(font);
 }
 
 bool msgDialog::eventFilter(QObject* obj, QEvent* evn) {
@@ -49,3 +49,83 @@ bool msgDialog::eventFilter(QObject* obj, QEvent* evn) {
 }
 
 void msgDialog::on_btnOK_clicked() { close(); }
+
+void msgDialog::on_hsHour_valueChanged(int value) {
+  QString strH;
+
+  QString strTime = QString("%1").arg(ui->hsHour->value(), 2, 10, QChar('0')) +
+                    ":" +
+                    QString("%1").arg(ui->hsMinute->value(), 2, 10, QChar('0'));
+  ui->dateTimeEdit->setTime(QTime::fromString(strTime, "HH:mm"));
+}
+
+void msgDialog::on_btnHour_clicked() { addBtn(0, 24, 5, "Hour"); }
+
+void msgDialog::on_btnMinute_clicked() { addBtn(0, 60, 6, "Minute"); }
+
+void msgDialog::on_btnDay_clicked() { addBtn(1, 31, 5, "Day"); }
+
+void msgDialog::on_btnMonth_clicked() { addBtn(1, 12, 3, "Month"); }
+
+void msgDialog::on_btnYear_clicked() { addBtn(2022, 15, 5, "Year"); }
+
+void msgDialog::addBtn(int start, int total, int col, QString flag) {
+  qDeleteAll(ui->frameSel->findChildren<QObject*>());
+
+  int row = 0;
+  int count = 0;
+
+  QGridLayout* gl = new QGridLayout(this);
+  gl->setMargin(10);
+  gl->setSpacing(10);
+  ui->frameSel->setLayout(gl);
+
+  for (int i = 0; i < total; i++) {
+    for (int j = 0; j < col; j++) {
+      QToolButton* btn = new QToolButton(ui->frameSel);
+      btn->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+      btn->setObjectName("btn" + QString::number(count));
+      btn->setText(QString::number(count + start));
+
+      connect(btn, &QToolButton::clicked, [=]() { onBtnClick(btn, flag); });
+
+      gl->addWidget(btn, row, j, 1, 1);
+      count++;
+      total--;
+    }
+    row++;
+  }
+
+  for (int i = 0; i < total; i++) {
+    QToolButton* btn = new QToolButton(ui->frameSel);
+    btn->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+    btn->setObjectName("btn" + QString::number(count + i));
+    btn->setText(QString::number(count + i + start));
+    connect(btn, &QToolButton::clicked, [=]() { onBtnClick(btn, flag); });
+    gl->addWidget(btn, row + 1, i, 1, 1);
+  }
+}
+
+void msgDialog::onBtnClick(QToolButton* btn, QString flag) {
+  QString title = btn->text();
+
+  QString strDT;
+  if (flag == "Year") {
+    y = title;
+  }
+  if (flag == "Month") {
+    m = title;
+  }
+  if (flag == "Day") {
+    d = title;
+  }
+  if (flag == "Hour") {
+    h = title;
+  }
+  if (flag == "Minute") {
+    mm = title;
+  }
+
+  strDT = y + "-" + m + "-" + d + " " + h + ":" + mm;
+  qDebug() << strDT;
+}
--- msgdialog.h
+++ msgdialog.h
@@ -2,6 +2,8 @@
 #define MSGDIALOG_H
 
 #include <QDialog>
+#include <QHBoxLayout>
+#include <QToolButton>
 
 namespace Ui {
 class msgDialog;
@@ -17,12 +19,28 @@ class msgDialog : public QDialog {
 
   void initDlg();
 
+  void addBtn(int start, int total, int col, QString flag);
+
  protected:
   bool eventFilter(QObject *obj, QEvent *evn) override;
  private slots:
   void on_btnOK_clicked();
 
+  void on_hsHour_valueChanged(int value);
+
+  void on_btnMinute_clicked();
+
+  void on_btnHour_clicked();
+
+  void on_btnDay_clicked();
+
+  void on_btnMonth_clicked();
+
+  void on_btnYear_clicked();
+
  private:
+  void onBtnClick(QToolButton *btn, QString flag);
+  QString y, m, d, h, mm;
 };
 
 #endif  // MSGDIALOG_H
