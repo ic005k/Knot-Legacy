93db2e691909c5ab3cfd5d0a7ba6b008e3f18a57
ic005k <zh99889@126.com>
2022-11-02 19:56:18 +0800

    Update mainwindow.cpp

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -5036,7 +5036,7 @@ void MainWindow::showGrayWindows() {
 
   QPropertyAnimation *m_pAnimation = new QPropertyAnimation();
   m_pAnimation->setTargetObject(m_widget);
-  m_pAnimation->setDuration(500);
+  m_pAnimation->setDuration(300);
   QGraphicsOpacityEffect *m_pOpacity = new QGraphicsOpacityEffect();
   m_widget->setGraphicsEffect(m_pOpacity);
   m_pOpacity->setOpacity(1);
