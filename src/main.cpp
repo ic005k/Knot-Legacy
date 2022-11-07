bae6dc5d7ac18e460ff931d85560b207ae06d98c
ic005k <zh99889@126.com>
2022-11-07 13:32:17 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,9 @@ int main(int argc, char* argv[]) {
   QtWebView::initialize();
   QApplication a(argc, argv);
 
+  QTextCodec* codec = QTextCodec::codecForName("UTF-8");
+  QTextCodec::setCodecForLocale(codec);
+
   //禁用文本选择（针对所有的可输入的编辑框）
   qputenv("QT_QPA_NO_TEXT_HANDLES", "1");
 
