cc9d1341c2fbc8caa0955c5adba53a1f4436c95f
ic005k <zh99889@126.com>
2023-12-19 16:58:17 +0800

    Update qtmaterialstatetransition.cpp

--- src/components/lib/qtmaterialstatetransition.cpp
+++ src/components/lib/qtmaterialstatetransition.cpp
@@ -1,19 +1,16 @@
-#include "lib/qtmaterialstatetransition.h"
+#include "src/components/lib/qtmaterialstatetransition.h"
 
-QtMaterialStateTransition::QtMaterialStateTransition(QtMaterialStateTransitionType type)
-    : m_type(type)
-{
+QtMaterialStateTransition::QtMaterialStateTransition(
+    QtMaterialStateTransitionType type)
+    : m_type(type) {}
+
+bool QtMaterialStateTransition::eventTest(QEvent *event) {
+  if (event->type() != QEvent::Type(QEvent::User + 1)) {
+    return false;
+  }
+  QtMaterialStateTransitionEvent *transition =
+      static_cast<QtMaterialStateTransitionEvent *>(event);
+  return (m_type == transition->type);
 }
 
-bool QtMaterialStateTransition::eventTest(QEvent *event)
-{
-    if (event->type() != QEvent::Type(QEvent::User + 1)) {
-        return false;
-    }
-    QtMaterialStateTransitionEvent *transition = static_cast<QtMaterialStateTransitionEvent *>(event);
-    return (m_type == transition->type);
-}
-
-void QtMaterialStateTransition::onTransition(QEvent *)
-{
-}
+void QtMaterialStateTransition::onTransition(QEvent *) {}
