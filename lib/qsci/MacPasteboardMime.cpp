2c64d5aabf3d204fcdb507ed1fe350f66a92e5ab
ic005k <zh99889@126.com>
2025-04-09 21:07:19 +0800

    Update MacPasteboardMime.cpp

--- lib/qsci/MacPasteboardMime.cpp
+++ lib/qsci/MacPasteboardMime.cpp
@@ -31,7 +31,8 @@
 #include <QStringList>
 #include <QVariant>
 
-#include <QMacPasteboardMime>
+// #include <QMacPasteboardMime>
+#include <qmacpasteboardmime.h>
 
 
 static const QLatin1String mimeRectangular("text/x-qscintilla-rectangular");
