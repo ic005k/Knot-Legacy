d9d9cb281e38962866b686fccb69ce26d71e9c68
ic005k <zh99889@126.com>
2025-04-13 19:28:10 +0800

    fix linux

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -3,7 +3,7 @@
 #include "src/onedrive/qtonedriveauthorizationdialog.h"
 #include "ui_MainWindow.h"
 
-QString ver = "1.2.31";
+QString ver = "1.2.32";
 QString appName = "Knot";
 
 QList<QPointF> PointList;
